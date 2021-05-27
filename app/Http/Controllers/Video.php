42e13a135f391f18f88d6e3c2787fae60e193f0a
saad-mm <m.saad@micromerger.com>
2021-05-27 17:51:15 +0500

    saad yoo

--- app/Http/Controllers/Video.php
+++ app/Http/Controllers/Video.php
@@ -18,7 +18,7 @@ class Video extends Controller
 	public function add_video(){
          return view('include/main_page', [
             'content' => 'video/add-video',
-			'user' => 'yoo',
+			'user' => 'yoo yoo',
 			
         ]); 
 	}
